6cf72407909e66fea90d6f352d4f0e13b48b3113
freewind <nowindlee@gmail.com>
2019-05-29 14:26:23 +0800

    demo

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-TypeScript "pdf.js" Disable Worker Demo
-========================================
+TypeScript "pdf.js" Worker Url From CDN Demo
+=============================================
 
-在pdfjs中，为了性能考虑，要求我们一定要提供某种方式的worker（比如一个worker的url，或者shared worker的port），
-否则的话，会报错：
-
-```
-No "GlobalWorkerOptions.workerSrc" specified.
-```
-
-但是在某些极端环境中，我们无法提供worker，终于找到了禁用worker的办法，就是给它一个错误的url，它获取不到的话，
-就会内部使用一个fake worker，利用浏览器的主线程来render。虽然慢，但是好在总算能用了。
+在webpack打包之后，使用cdn的worker url
 
 ```
 npm install
--- entry.ts
+++ entry.ts
@@ -1,9 +1,10 @@
-import pdfjs, {PDFPageProxy} from 'pdfjs-dist';
+import pdfjs, {PDFDocumentProxy} from 'pdfjs-dist';
 import 'pdfjs-dist/build/pdf.worker';
-import {PDFDocumentProxy} from 'pdfjs-dist';
 import './node_modules/pdfjs-dist/web/pdf_viewer.css';
 
-const {PDFViewer, PDFPageView} = require('pdfjs-dist/web/pdf_viewer.js');
+const {PDFViewer} = require('pdfjs-dist/web/pdf_viewer.js');
+
+const workerUrl = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.min.js';
 
 // Notice:
 // set document to viewer directly may freeze browser if the pdf is big
@@ -12,50 +13,13 @@ function renderPdfInAll(pdfDocument: PDFDocumentProxy) {
     container: document.getElementById('viewerContainer'),
   });
 
-  document.addEventListener('pagesinit', function () {
-    // We can use pdfViewer now, e.g. let's change default scale.
-    // pdfViewer.currentScaleValue = 'page-width';
-  });
-
   pdfViewer.setDocument(pdfDocument);
 }
 
-async function delay(ms: number): Promise<void> {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
-  })
-}
-
-// Notice
-// render pages one by one and give some delay time between them
-// will give some time to browser to handle events, is better
-async function renderPages(pdfDocument: PDFDocumentProxy) {
-  await renderPage(pdfDocument, 1);
-}
-
-async function renderPage(pdfDocument: PDFDocumentProxy, index: number) {
-  if (index > pdfDocument.numPages) {
-    return;
-  }
-
-  const pdfPage = await pdfDocument.getPage(index);
-  const pdfPageViewer = new PDFPageView({
-    container: document.getElementById('viewerContainer'),
-    defaultViewport: pdfPage.getViewport(1),
-  });
-
-  pdfPageViewer.setPdfPage(pdfPage);
-  await pdfPageViewer.draw()
-  await delay(100);
-  await renderPage(pdfDocument, index + 1);
-}
-
 async function loadPdf() {
-  (pdfjs as any).GlobalWorkerOptions.workerSrc = 'no-worker-can-provided';
+  (pdfjs as any).GlobalWorkerOptions.workerSrc = workerUrl;
   const pdf = await pdfjs.getDocument('/big.pdf').promise
-  await renderPages(pdf);
+  await renderPdfInAll(pdf);
 }
 
 loadPdf();
