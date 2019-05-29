d7afdd62f7d21266f48b917990d9f23d91845beb
freewind <nowindlee@gmail.com>
2019-05-29 14:09:23 +0800

    use url-loader to handle images in css

--- webpack.config.ts
+++ webpack.config.ts
@@ -28,16 +28,7 @@ const config: Configuration = {
       }]
     }, {
       test: /\.(gif|png|jpe?g|svg)$/i,
-      use: [
-        'file-loader',
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            bypassOnDebug: true, // webpack@1.x
-            disable: true, // webpack@2.x and newer
-          },
-        },
-      ],
+      loader: 'url-loader',
     }]
   },
   plugins: [
